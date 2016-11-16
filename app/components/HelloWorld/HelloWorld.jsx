c0c18853c5b70ee385958d8ae060257d89447a94
Matthew Toledo <mtoledo@express.com>
2016-11-16 16:42:41 -0500

    ditching transform-react-jsx-img-import
    Ditching inline image imports.  It was preventing us from leveraging es6 import tree shaking.  It’s just one more transpilation that could go wrong.

--- app/components/HelloWorld/HelloWorld.jsx
+++ app/components/HelloWorld/HelloWorld.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import style from './helloworld.scss';
 import MountainSvg from './mountain.svg';
+import gif from './popeye.gif';
+import png from './mountain.png';
+import jpg from './mountain.jpg';
 
 class HelloWorld extends React.Component {
 
@@ -17,13 +20,13 @@ class HelloWorld extends React.Component {
         <h1 className={style.header}>{this.state.message}</h1>
         <ul className={style.imageList}>
           <li>
-            <img src="./mountain.jpg" width={100} height={100} alt="jpg test" />
+            <img src={jpg} width={100} height={100} alt="jpg test" />
           </li>
           <li>
-            <img src="./mountain.png" width={100} height={100} alt="png test" />
+            <img src={png} width={100} height={100} alt="png test" />
           </li>
           <li>
-            <img src="./popeye.gif" width={100} height={100} alt="gif test" />
+            <img src={gif} width={100} height={100} alt="gif test" />
           </li>
           <li>
             <MountainSvg width={100} height={100} alt="svg test" />
