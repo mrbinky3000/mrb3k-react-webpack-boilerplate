ff8de511ad1be9dc11d0d26cd478e3145c83a3d3
Matthew Toledo <mtoledo@express.com>
2017-01-23 12:49:37 -0500

    minor lint issues with tests

--- tests/HelloWorld.jsx
+++ tests/HelloWorld.jsx
@@ -5,12 +5,12 @@ import HelloWorld from '../app/components/HelloWorld';
 describe('<HelloWorld>', () => {
   it('should render a H1 tag', () => {
     expect(
-      render(<HelloWorld />).find('h1').length
+      render(<HelloWorld />).find('h1').length,
     ).toBe(1);
   });
   it('should say "Hello World!" inside of the H1 tag', () => {
     expect(
-      render(<HelloWorld />).find('h1').text()
+      render(<HelloWorld />).find('h1').text(),
     ).toBe('Hello World!');
   });
 });
